1ee389aa20e2267a9c023a652691f90aec005eca
cubodado <dalkang000@gmail.com>
2022-04-27 17:47:23 +0900

    Feat: create and apply TodoInput component

---
+++ src/components/TodoInput.js
@@ -0,0 +1,41 @@
+import classes from './TodoInput.module.css';
+
+const TodoInput = () => {
+  const isEnter = (e) => {
+    const todoInput = document.querySelector('input');
+    if (e.charCode === 13) {
+      isValidTodo(todoInput);
+    }
+  };
+
+  const isAllString = (todoInput) => {
+    const r = / /gi;
+    const todoInputLength = todoInput.replace(r, '').length;
+    return !!todoInputLength;
+  };
+
+  const isValidTodo = (todoInput) => {
+    if (isAllString(todoInput.value) === false) {
+      const ALERT_MESSAGE = '공백이 아닌 글자를 입력해 주세요.';
+      alert(ALERT_MESSAGE);
+      todoInput.value = '';
+      return;
+    }
+
+    const validTodoInput = todoInput.value.trim();
+
+    console.log(validTodoInput);
+
+    todoInput.value = '';
+  };
+
+  return (
+    <input
+      className={classes.input}
+      placeholder="Write your todo📝 and press enter ⌨️"
+      onKeyPress={isEnter}
+    ></input>
+  );
+};
+
+export default TodoInput;
