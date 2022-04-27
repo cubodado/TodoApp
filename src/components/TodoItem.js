3663d4e85ecee5aa0006b4a48c1a342cc9e66732
cubodado <dalkang000@gmail.com>
2022-04-27 20:50:17 +0900

    Update: toggle clear state

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,28 @@
 import classes from './TodoItem.module.css';
+import { useDispatch } from 'react-redux';
+import { toggleClear } from '../redux/todoSlice';
 
 const TodoItem = ({ id, content, clear }) => {
+  const dispatch = useDispatch();
+
+  const toggleClearHandler = () => {
+    dispatch(
+      toggleClear({
+        id: id,
+        clear: !clear,
+      })
+    );
+  };
+
   return (
-    <div className={classes.item}>
+    <div className={`${classes.item} ${clear && classes.checked}`}>
       <label>
-        <input type="checkbox" name="todo" key={id} clear={clear}></input>
+        <input
+          type="checkbox"
+          checked={clear}
+          onChange={toggleClearHandler}
+          key={id}
+        ></input>
         <p>{content}</p>
       </label>
     </div>
--- src/redux/todoSlice.js
+++ src/redux/todoSlice.js
@@ -3,10 +3,10 @@ import { createSlice } from '@reduxjs/toolkit';
 const todoSlice = createSlice({
   name: 'todos',
   initialState: [
-    { id: 1, content: 'Meditation', clear: 'false' },
-    { id: 2, content: 'Taking a nap', clear: 'true' },
-    { id: 3, content: 'Reading network books', clear: 'false' },
-    { id: 4, content: 'Running', clear: 'false' },
+    { id: 1, content: 'Meditation', clear: false },
+    { id: 2, content: 'Taking a nap', clear: true },
+    { id: 3, content: 'Reading network books', clear: false },
+    { id: 4, content: 'Running', clear: false },
   ],
   reducers: {
     addTodo: (state, action) => {
@@ -17,8 +17,12 @@ const todoSlice = createSlice({
       };
       state.push(newTodo);
     },
+    toggleClear: (state, action) => {
+      const index = state.findIndex((todo) => todo.id === action.payload.id);
+      state[index].clear = action.payload.clear;
+    },
   },
 });
 
-export const { addTodo } = todoSlice.actions;
+export const { addTodo, toggleClear } = todoSlice.actions;
 export default todoSlice.reducer;
